b0abd43ae27904592e3422951c747b4761adda00
chouzzy <mathfernando@hotmail.com>
2025-10-07 19:12:37 -0300

    email split 2

--- src/modules/management/useCases/inviteUser/InviteUserUseCase.ts
+++ src/modules/management/useCases/inviteUser/InviteUserUseCase.ts
@@ -15,7 +15,7 @@ class InviteUserUseCase {
     async execute({ email, name, initialRole }: IRequest): Promise<void> {
         // Passo 1: Cria o utilizador no Auth0.
         // O Auth0 tratará de enviar o e-mail de verificação, que serve como convite.
-        const { newUser, ticketUrl } = await auth0ManagementService.createUserAndSendMagicLink(email, name);
+        const { newUser, ticketUrl } = await auth0ManagementService.createUser(email, name);
 
         console.log(`[INVITE USER] Link de criação de senha para ${email}: ${ticketUrl}`);
 
