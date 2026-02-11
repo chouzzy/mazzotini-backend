f9449551d793a846e75909ba5d7827713178f724
chouzzy <mathfernando@hotmail.com>
2026-02-11 20:17:11 -0300

    teste workflows 8

---
+++ src/@types/express-fix.d.ts
@@ -0,0 +1,20 @@
+import * as core from 'express-serve-static-core';
+
+// Este bloco "reabre" a definição do Express e força os tipos 
+// a serem aceitos como strings simples, resolvendo o erro globalmente.
+
+declare module 'express-serve-static-core' {
+  interface ParamsDictionary {
+    [key: string]: string;
+  }
+  interface Query {
+    [key: string]: any;
+  }
+}
+
+declare module 'express' {
+  interface Request {
+    params: core.ParamsDictionary;
+    query: core.Query;
+  }
+}
